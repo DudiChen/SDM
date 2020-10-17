7ed5240fd114327549d0d06c1931c051650f409e
Noam Waissman <noam.waissman@amdocs.com>
2020-10-17 22:28:49 +0300

    initial commit with build product of client application and test endpoint servlet

---
+++ out/artifacts/SDM_war_exploded/precache-manifest.ba81472e1261fdfda8e2e38e880abf36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d9c5ecafb08840c88d5def3d27e68c5",
+    "url": "/SDM/App/index.html"
+  },
+  {
+    "revision": "ab7ae4691570ecbd1f87",
+    "url": "/SDM/App/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b8461bcd469f56fce672",
+    "url": "/SDM/App/static/js/2.70742dab.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/SDM/App/static/js/2.70742dab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab7ae4691570ecbd1f87",
+    "url": "/SDM/App/static/js/main.acd8a135.chunk.js"
+  },
+  {
+    "revision": "538c79e6f6593d34a575",
+    "url": "/SDM/App/static/js/runtime-main.fdeb9eb7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SDM/App/static/media/logo.5d5d9eef.svg"
+  }
+]);
