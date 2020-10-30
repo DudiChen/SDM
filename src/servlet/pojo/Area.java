38cc3fddb4ccc6f8f4928e360740424d256dae80
Noam Waissman <noamwa@amdocs.com>
2020-10-30 18:24:14 +0200

    added transaction, area, user pojos

---
+++ src/servlet/pojo/Area.java
@@ -0,0 +1,10 @@
+package servlet.pojo;
+
+public class Area {
+    String name;
+    String owner;
+    int totalProducts;
+    int totalStores;
+    int totalOrders;
+    int avgOrderCost;
+}
---
+++ src/servlet/pojo/Transaction.java
@@ -0,0 +1,11 @@
+package servlet.pojo;
+
+import java.util.Date;
+
+public class Transaction {
+    String type;
+    Date date;
+    int amount;
+    int balanceBefore;
+    int balanceAfter;
+}
