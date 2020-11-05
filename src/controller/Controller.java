620c849ee9af94415129e7d07fdcc62035dea613
noamwa <noamwa95@gmail.com>
2020-11-05 19:45:36 +0200

    cleared redundant controller code

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,8 +1,5 @@
 package controller;
-
 import builder.AreaBuilder;
-//import builder.MarketBuilder;
-//import command.Executor;
 import entity.*;
 import entity.Area;
 import entity.OrderInvoice;
@@ -12,11 +9,6 @@ import exception.OrderValidationException;
 import exception.XMLException;
 import javafx.util.Pair;
 import jaxb.JaxbHandler;
-//import view.ApplicationContext;
-//import view.View;
-//import view.menu.item.CustomerMapElement;
-//import view.menu.item.StoreMapElement;
-
 import javax.management.modelmbean.XMLParseException;
 import javax.websocket.Session;
 import javax.xml.bind.ValidationException;
@@ -29,17 +21,13 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Controller {
-    //    private Executor executor;
-    private boolean loaded = false;
     private Market market;
-    private int currentCustomerId;
     private static Controller instance;
     Map<Customer, Session> loggedInSellerToSession;
 
     private Controller() {
         loggedInSellerToSession = Collections.synchronizedMap(new HashMap<Customer, Session>());
         this.market = new Market();
-//        this.executor = new Executor(this);
     }
 
     public static Controller getInstance() {
@@ -49,36 +37,11 @@ public class Controller {
         return instance;
     }
 
-    public void fetchAllStoresToUI() {
-//        List<Store> stores = new ArrayList<>();
-//        if (market == null || market.isEmpty()) {
-//            view.displayStores(stores);
-//            return;
-//        }
-//        stores = this.market.getAllStores();
-//        view.displayStores(stores);
-    }
-
     public int getProductNumberOfSales(int areaId, int productId) {
         return this.market.getAreaById(areaId).getAllStores().stream()
                 .map(store -> store.getTotalProductSales(productId))
                 .reduce(0, Integer::sum);
     }
-
-//    public void fetchAllStoresToUI() {
-////        List<Store> stores = new ArrayList<>();
-////        if (market == null || market.isEmpty()) {
-////            view.displayStores(stores);
-////            return;
-////        }
-////        stores = this.market.getAllStores();
-////        view.displayStores(stores);
-//    }
-
-//    public void loadXMLDataToUI() {
-//        String xmlPath = view.promptUserFilePath();
-//        loadXMLData(xmlPath);
-//    }
 
     public String loadXMLData(File xmlFile, int currentCustomerId) {
         Area area;
@@ -93,70 +56,7 @@ public class Controller {
             message = e.getMessage();
         }
         return message;
-//        LoadXmlTask loadXmlTask = new LoadXmlTask(fullFilePath);
-//        loadXmlTask.setOnSucceeded(event -> {
-//            try {
-//                this.market = loadXmlTask.get();
-//            } catch (InterruptedException | ExecutionException e) {
-//                e.printStackTrace();
-//            }
-//            view.fileLoadedSuccessfully();
-//            view.showMainMenu();
-//        });
-//        loadXmlTask.setOnFailed(event -> {
-//            Throwable exception = loadXmlTask.getException();
-//            if (exception instanceof ValidationException) {
-//                view.displayError(exception.getMessage());
-//            }
-//            else if (exception instanceof XMLParseException || exception instanceof XMLException || exception instanceof FileNotFoundException) {
-//                view.displayError("File Not Found");
-//            } else {
-//                view.displayError("Couldn't Parse XML due to unknown error:" + exception.getMessage() + System.lineSeparator());
-//            }
-//        });
-//        this.bindTaskToUIComponents(loadXmlTask);
-//        new Thread(loadXmlTask).start();
     }
-
-//    private void bindTaskToUIComponents(LoadXmlTask loadXmlTask) {
-//        // task message
-//        view.xmlProgressStateProperty().bind(loadXmlTask.messageProperty());
-//        view.xmlProgressBarProperty().bind(loadXmlTask.progressProperty());
-//
-//        loadXmlTask.valueProperty().addListener((observable, oldValue, newValue) -> {
-//            view.xmlProgressStateProperty().unbind();
-//            view.xmlProgressBarProperty().unbind();
-//        });
-//    }
-
-//    public void addNewProduct(int storeId, int productId) {
-//        this.market.addProductToStore(storeId, productId, 0);
-//        this.view.onStoreIdChoice.accept(storeId);
-//    }
-
-//    public void fetchAllProductsToUI() {
-//        List<Product> products = new ArrayList<>();
-//        List<Store> allStores = new ArrayList<>();
-//        if (market == null || market.isEmpty()) {
-//            view.displayProducts(products, allStores);
-//            return;
-//        }
-//        products = market.getAllProducts();
-//        allStores = market.getAllStores();
-//        view.displayProducts(products, allStores);
-//    }
-
-
-//    public void getCustomerToUI() {
-//    }
-
-//    private void registerToViewEvents() {
-//        registerOnStoreChoice();
-//        registerOnOrderPlaced();
-//        registerOnOrderAccepted();
-//        registerOnOrderCanceled();
-//        registerOnDynamicOrder();
-//    }
 
     private Optional<List<Pair<Integer, Double>>> findCheapestOrderForStore(Area area, Store store, List<Pair<Integer, Double>> productQuantityPairs) {
 
@@ -186,36 +86,12 @@ public class Controller {
                             });
                 });
         if (res.isEmpty()) {
-        return Optional.empty();
+            return Optional.empty();
         }
         System.out.println(res);
         return Optional.of(res);
     }
 
-//    private void registerOnOrderAccepted() {
-//        // view.onOrderAccepted = orderInvoiceId -> market.approveOrder(orderInvoiceId);
-//    }
-//
-//    private void registerOnOrderCanceled() {
-//        // view.onOrderCanceled = (orderInvoiceId) -> market.cancelOrder(orderInvoiceId);
-//    }
-//
-//    private void registerOnStoreChoice() {
-////        view.onStoreIdChoice = (storeId) -> {
-////        assert false;
-////        chosenStore.set(market.getStoreById(storeId));
-////        fetchAllProductsListToUI();
-// //       };
-//    }
-//
-//    private void registerOnOrderPlaced() {
-//        // view.onOrderPlaced = this::makeOrderForChosenStore;
-//    }
-
-    //    TODO::UI: Missing the Offers support from order when displaying invoice.
-//    TODO::UI: in the Order History, under Orders tab; the Discount offer items are not counted - need to add as "Number of items from Discounts" => use getNumberOfDiscountProducts() & getNumberOfInvoiceProducts().
-//    TODO::UI: Orders does not display customer related data
-//    TODO::UI: On Dynamic Order display; missing prompt to user about order split info before viewing multiple orders
     private int makeOrderForStore(Area area, Store store, Date date, Integer customerId, Pair<List<Pair<Integer, Double>>, List<Discount.Offer>> productQuantityPairsWithOffers) throws OrderValidationException {
         StringBuilder err = new StringBuilder();
         List<Discount.Offer> chosenOffers = productQuantityPairsWithOffers.getValue();
@@ -241,172 +117,12 @@ public class Controller {
         storeOwner.addNotification(orderingCustomer.getName() + " ordered from " + store.getName());
         this.notifyLoggedInSellers(storeOwner);
         return orderId;
-//        view.summarizeOrder(market.getOrderInvoice(orderInvoiceId));
-//    }
-//
-//    // TODO :: make an alert when dynamic order is more than 2 stores long
-//    public void makeDynamicOrder() {
-//        fetchAllProductsListToUI();
     }
-
-//    public void makeOrder() {
-//        fetchAllStoresListToUI();
-//    }
-
-//    public void fetchAllStoresListToUI() {
-//        List<Store> stores = new ArrayList<>();
-//        if (market == null || market.isEmpty()) {
-//            view.displayStoresList(stores);
-//            return;
-//        }
-//        stores = market.getAllStores();
-//        view.displayStoresList(stores);
-//    }
-
-//    public void fetchAllProductsListToUI() {
-//        List<Product> products = new ArrayList<>();
-//        List<Store> allStores = new ArrayList<>();
-//        if (market == null || market.isEmpty()) {
-//            view.displayProductsForStore(products, chosenStore.get());
-//            return;
-//        }
-//        products = market.getAllProducts()
-//                .stream().filter(product -> chosenStore.get().isProductSold(product.getId())).collect(Collectors.toList());
-//        allStores = market.getAllStores();
-//        view.displayProductsForStore(products, chosenStore.get());
-//    }
-//
-//    public void fetchOrdersHistoryToUI() {
-//        try {
-//            view.showOrdersHistory(market.getOrdersHistory());
-//        } catch (MarketIsEmptyException e) {
-//            view.displayError("Please Load XML First");
-//        }
-//    }
-//
-//    public Executor getExecutor() {
-//        return executor;
-//    }
-//
-//    public void run() {
-//        this.view.showMainMenu();
-//    }
-
-//    public Store getStoreById(int storeId) {
-//        return this.market.getStoreById(storeId);
-//    }
-
-//    public String getStoreNameByID(int storeId) {
-//        return this.market.getStoreById(storeId).getName();
-//    }
-
-    public void saveOrderHistory() {
-//        if (market.isEmpty()) { // TODO: this is a patch - the check should be if the market has the sufficient data - fix for ex2 !
-//            view.displayError("Please Load XML File Before");
-//            return;
-//        }
-//        try {
-//            List<OrderInvoice> history = market.getOrdersHistory();
-//            if (history.size() == 0) {
-//                view.displayError("No History To Show");
-//            } else {
-//                String outputPath = view.promptUserFilePath();
-//                try (OutputStream outputStream = new FileOutputStream(outputPath)) {
-//                    ObjectOutputStream out = new ObjectOutputStream(outputStream);
-//                    out.writeObject(history);
-//                    view.fileLoadedSuccessfully();
-//                } catch (FileNotFoundException e) {
-//                    view.displayError("File Not Found");
-//
-//                } catch (IOException e) {
-//                    view.displayError("Error While Writing To File");
-//                }
-//            }
-//        } catch (MarketIsEmptyException e) {
-//            view.displayError("Load XML First");
-//        }
-
-    }
-
-    public void loadOrderHistory() {
-//        if (market.isEmpty()) { // TODO: this is a patch - the check should be if the market has the sufficient data - fix for ex2 !
-//            view.displayError("Please Load XML File Before");
-//            return;
-//        }
-//        String inputPath = view.promptUserFilePath();
-//        try (InputStream inputStream = new FileInputStream(inputPath)) {
-//            ObjectInputStream in = new ObjectInputStream(inputStream);
-//            List<OrderInvoice> history = (ArrayList<OrderInvoice>) in.readObject();
-//            market.setOrdersHistory(history);
-//            view.fileLoadedSuccessfully();
-//        } catch (FileNotFoundException e) {
-//            view.displayError("File Not Found");
-//        } catch (IOException e) {
-//            view.displayError("Error While Writing To File");
-//        } catch (ClassNotFoundException e) {
-//            e.printStackTrace();
-//        }
-    }
-
-
-//    public void fetchMapToUI() {
-//        this.view.showMap(
-//                Stream.concat(
-//                        this.market.getAllStores().stream()
-//                                .map(store -> new StoreMapElement(store, view.onStoreIdChoice)),
-//                        this.market.getAllCustomers().stream()
-//                                .map(customer -> new CustomerMapElement(customer))
-//                )
-//                        .collect(Collectors.toList()));
-//    }
-
-//    public Product getProductById(int productId) {
-//        return this.market.getProductById(productId);
-//    }
 
     public Product getAreaProductById(int areaId, int productId) {
         return this.market.getAreaProductById(areaId, productId);
     }
 
-//    public boolean isAvailableDiscount(Discount discount, List<Pair<Integer, Double>> orderProducts, List<Discount> chosenDiscounts) {
-//        int timesUsedDiscount = Collections.frequency(chosenDiscounts, discount);
-//        boolean isAvailable = this.market.isAvailableDiscount(discount, orderProducts, timesUsedDiscount);
-//        if (!isAvailable) {
-//            view.displayMessage(
-//                    String.format("%s: Not eligible %s according to current product quantity.",
-//                            discount.getName(),
-//                            (timesUsedDiscount > 0) ? "for another discount" : ""));
-//        }
-//        return isAvailable;
-//    }
-//
-//    public List<Customer> getAllCustomers() {
-//        return this.market.getAllCustomers();
-//    }
-//
-//    public Customer getCustomerById(int customerId) {
-//        return this.market.getCustomerById(customerId);
-//    }
-
-//    public void deleteProduct(int productId, int storeId) {
-//        try {
-//            this.market.deleteProductForStore(productId, storeId);
-//            ApplicationContext.getInstance().navigateBack();
-//            this.view.onStoreIdChoice.accept(storeId);
-//        } catch (ValidationException | DiscountsRemovedException e) {
-//            this.view.displayMessage(e.getMessage());
-//        }
-//    }
-
-//    public void changePriceForProduct(int storeId, int productId, double newPrice) {
-//        this.market.changePriceForProduct(storeId, productId, newPrice);
-//    }
-
-//    public List<Product> getAllProducts() {
-//        return this.market.getAllProducts();
-//    }
-
-    // ex3
     public Customer getCustomerById(int uuid) {
         return this.market.getCustomerById(uuid);
     }
@@ -572,7 +288,7 @@ public class Controller {
 
         Pair<List<Pair<Integer, Double>>, List<Discount.Offer>> productQuantityPairs = new Pair<>(productIdQuantityPairs, new ArrayList<>());
         List<Integer> orderIds = new ArrayList<>();
-        for(Store store : this.market.getAreaById(areaId).getAllStores()) {
+        for (Store store : this.market.getAreaById(areaId).getAllStores()) {
             Optional<List<Pair<Integer, Double>>> maybeOrder = findCheapestOrderForStore(this.getAreaById(areaId), store, productQuantityPairs.getKey());
             if (maybeOrder.isPresent()) {
                 List<Pair<Integer, Double>> orderPairs = maybeOrder.get();
